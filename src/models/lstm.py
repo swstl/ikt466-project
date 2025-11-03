5935886cca1799e1037d0861d7ba8562f1580da4
Erlend Tregde <erlendt@uia.no>
2025-11-03 20:32:04 +0100

    lstm model

---
+++ src/models/lstm.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+
+class LSTM(nn.Module):
+    def __init__(self, input_size=13, hidden_size=128, num_layers=2, num_classes=10, dropout=0.3):
+        super(LSTM, self).__init__()
+        
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.lstm_layer = nn.LSTM(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            batch_first=True,
+            dropout=dropout if num_layers > 1 else 0
+        )
+        
+        self.classifier = nn.Sequential(
+            nn.Dropout(dropout),
+            nn.Linear(hidden_size, num_classes)
+        )
+    
+    def forward(self, x):
+        lstm_out, (h_n, c_n) = self.lstm_layer(x)
+
+        out = h_n[-1]
+
+        out = self.classifier(out)
+        
+        return out
